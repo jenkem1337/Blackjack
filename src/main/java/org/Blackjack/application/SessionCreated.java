81f4bd187385617f93a5eff230773de8e713fb03
Hasancan Sahan <yamahasirossi@gmail.com>
2025-12-17 14:48:56 +0300

    RoomId changed with UUID

--- src/main/java/org/Blackjack/application/SessionCreated.java
+++ src/main/java/org/Blackjack/application/SessionCreated.java
@@ -3,5 +3,7 @@ package org.Blackjack.application;
 import org.Blackjack.domain.PlayerID;
 import org.Blackjack.infrastructure.Response;
 
-public record SessionCreated(PlayerID roomOwnerId, RoomId roomId, GameRoomState gameRoomState) implements Response {
+import java.util.UUID;
+
+public record SessionCreated(PlayerID roomOwnerId, UUID roomId, GameRoomState gameRoomState) implements Response {
 }
