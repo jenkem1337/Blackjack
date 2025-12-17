cd098d542e6a09512c80255905afb66b57406491
Hasancan Sahan <yamahasirossi@gmail.com>
2025-12-17 14:45:53 +0300

    Create ActorContext.java

---
+++ src/main/java/org/Blackjack/infrastructure/ActorContext.java
@@ -0,0 +1,16 @@
+package org.Blackjack.infrastructure;
+
+public class ActorContext {
+    private final ActorSystem system;
+    private final ActorCell self;
+
+    ActorContext(ActorSystem system, ActorCell self) {
+        this.system = system;
+        this.self = self;
+    }
+
+    public ActorRef fork(AbstractActor childActor) {
+        return system.fork(childActor, self);
+    }
+
+}
