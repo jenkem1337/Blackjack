e5e96945543e110086dcdb4d459257206757bbbb
Hasancan Sahan <yamahasirossi@gmail.com>
2025-12-17 14:48:39 +0300

    some refactoring

--- src/main/java/org/Blackjack/infrastructure/ActorSystem.java
+++ src/main/java/org/Blackjack/infrastructure/ActorSystem.java
@@ -1,17 +1,42 @@
 package org.Blackjack.infrastructure;
 
+import java.util.Map;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ActorSystem {
-//    private final ActorRouter router;
+    private final Map<ActorRef, ActorCell> registry = new ConcurrentHashMap<>();
 
-    public ActorRef fork(AbstractActor<?> actor, Queue<Command> mailbox, WaitStrategy waitStrategy, MessageProcessorFunction callback) {
 
-        return ActorRef.createDefaultActorRef(actor, mailbox, waitStrategy, callback);
+    public ActorRef fork(AbstractActor actor, ActorCell parent) {
+        Queue<Command> mailbox = new ConcurrentLinkedQueue<>();
+        ActorCell cell = new ActorCell(
+                this,
+                actor,
+                mailbox,
+                new ThreadYieldWaitStrategy(),
+                new DefaultMailboxMessageProcessor(),
+                parent
+        );
+
+        ActorRef ref = new DefaultActorRef(cell);
+        registry.put(ref, cell);
+        if(parent != null) {
+            parent.addChild(cell);
+        }
+        cell.start();
+        return ref;
     }
 
-    public ActorRef fork(AbstractActor<?> actor) {
-        return ActorRef.createDefaultActorRef(actor, new ConcurrentLinkedQueue<>(), new ThreadYieldWaitStrategy(), new DefaultMailboxMessageProcessor());
+    public ActorRef fork(AbstractActor actor) {
+        return fork(actor, null);
+    }
+    public void stop(ActorRef actorRef) {
+        ActorCell cell = registry.remove(actorRef);
+        if (cell != null) {
+            cell.stop();
+        }
+
     }
 }
