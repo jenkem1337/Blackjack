fe37d055e7a73326eea82a4f0644c363008cab9f
Hasancan Sahan <yamahasirossi@gmail.com>
2025-12-17 14:49:31 +0300

    DefaultActorRef simplified with ActorCell object

--- src/main/java/org/Blackjack/infrastructure/DefaultActorRef.java
+++ src/main/java/org/Blackjack/infrastructure/DefaultActorRef.java
@@ -4,46 +4,25 @@ import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 
 public class DefaultActorRef implements ActorRef {
-    private final Actor<Command, Response> actor;
-    private final Thread mailboxThread;
-    private final Queue<Command> mailbox;
-    private final WaitStrategy waitStrategy;
-    private volatile boolean isRunning;
-    DefaultActorRef(Actor<Command, Response> actor, Queue<Command> mailbox, WaitStrategy waitStrategy, MessageProcessorFunction messageProcessingCallback) {
-        this.actor = actor;
-        this.isRunning = true;
-        this.waitStrategy = waitStrategy;
-        this.mailbox = mailbox;
-        this.mailboxThread = Thread.ofVirtual()
-                .unstarted(
-                        messageProcessingCallback
-                                .apply(
-                                        this.isRunning,
-                                        this.actor,
-                                        this.waitStrategy,
-                                        this.mailbox
-                                )
-                );
+    private final ActorCell actorCell;
+    DefaultActorRef(ActorCell actorCell) {
+        this.actorCell = actorCell;
     }
+
     @Override
     public CompletableFuture<Response> send(Command msg) {
-        mailbox.offer(msg);
+        actorCell.enqueue(msg);
         return msg.future();
     }
-    
+
     @Override
     public void start() {
-        isRunning = true;
-        if(!mailboxThread.isAlive()){
-            mailboxThread.start();
-        }
+        actorCell.start();
     }
 
     @Override
     public void stop() {
-        isRunning = false;
-        if(!mailboxThread.isInterrupted()){
-            mailboxThread.interrupt();
-        }
+        actorCell.stop();
     }
+
 }
