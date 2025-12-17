dfdd738a4739142c93ed5c6930b0f24408c1e2f9
Hasancan Sahan <yamahasirossi@gmail.com>
2025-12-17 14:48:15 +0300

    static factory methods removed

--- src/main/java/org/Blackjack/infrastructure/ActorRef.java
+++ src/main/java/org/Blackjack/infrastructure/ActorRef.java
@@ -7,16 +7,6 @@ interface ActorRef {
 
     CompletableFuture<Response> send(Command msg);
 
-    static ActorRef createDefaultActorRefAndStartImmediately(AbstractActor<?> actor, Queue<Command> queue, WaitStrategy waitStrategy, MessageProcessorFunction mailboxThreadExecutorCallback) {
-        actor.setSelfQueue(queue);
-        var mailbox =  new DefaultActorRef(actor, queue, waitStrategy, mailboxThreadExecutorCallback);
-        mailbox.start();
-        return mailbox;
-    }
-    static ActorRef createDefaultActorRef(AbstractActor<?> actor, Queue<Command> mailbox, WaitStrategy waitStrategy, MessageProcessorFunction mailboxThreadExecutorCallback) {
-        actor.setSelfQueue(mailbox);
-        return new DefaultActorRef(actor, mailbox, waitStrategy, mailboxThreadExecutorCallback);
-    }
     void start();
     void stop();
 }
