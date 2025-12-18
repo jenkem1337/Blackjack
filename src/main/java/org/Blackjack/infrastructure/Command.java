74374a4d8d293db632c0418d7d0319caa521cd93
Hasancan Sahan <yamahasirossi@gmail.com>
2025-12-18 17:55:40 +0300

    added generic and command method

--- src/main/java/org/Blackjack/infrastructure/Command.java
+++ src/main/java/org/Blackjack/infrastructure/Command.java
@@ -2,7 +2,8 @@ package org.Blackjack.infrastructure;
 
 import java.util.concurrent.CompletableFuture;
 
-public interface Command {
+public interface Command<T> {
     void complete(Response responseMessage);
     CompletableFuture<Response> future();
+    T command();
 }
