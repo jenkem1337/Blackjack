132e1a8fb33371e0c3787500c4b853f3f6f3c4d1
Hasancan Sahan <yamahasirossi@gmail.com>
2025-12-26 15:03:45 +0300

    selfEnqueue methods added

--- src/main/java/org/Blackjack/infrastructure/AbstractActor.java
+++ src/main/java/org/Blackjack/infrastructure/AbstractActor.java
@@ -3,6 +3,7 @@ package org.Blackjack.infrastructure;
 import java.util.Queue;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 public abstract class AbstractActor implements Actor<Command, Response>{
     private final UUID id = UUID.randomUUID();
@@ -13,7 +14,28 @@ public abstract class AbstractActor implements Actor<Command, Response>{
         if(queue == null) throw new IllegalArgumentException("Queue must not be null !");
         this.selfQueue = queue;
     }
-    public void selfEnqueue(CompletableFuture<Command> future) {
+    public void selfEnqueue(CompletableFuture<Response> future) {
+        future.whenComplete(((response, throwable) -> {
+                if(throwable != null) {
+                    selfQueue.offer(new AsyncCommand(new InternalErrorCommand(throwable.getMessage())));
+                    return;
+                }
+                selfQueue.offer(new AsyncCommand(response));
+        }));
+    }
+    public void  selfEnqueueWithExternalFuture(
+            CompletableFuture<Response> future,
+            CompletableFuture<?> externalFuture,
+            Function<Response,?> responseToCommandMapper) {
+        future.whenComplete(((response, throwable) -> {
+            if(throwable != null) {
+                selfQueue.offer(new AsyncCommand(new InternalErrorCommand(throwable.getMessage()), externalFuture));
+                return;
+            }
+            var command = responseToCommandMapper.apply(response);
+            selfQueue.offer(new AsyncCommand(command, externalFuture));
+        }));
+
     }
     void attachContext(ActorContext context) {
         this.context = context;
