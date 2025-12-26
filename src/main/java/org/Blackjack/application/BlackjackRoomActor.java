616c5a54bb4c50371b1aa7a74043b9651b6304af
Hasancan Sahan <yamahasirossi@gmail.com>
2025-12-26 15:05:59 +0300

    some refactoring

--- src/main/java/org/Blackjack/application/BlackjackRoomActor.java
+++ src/main/java/org/Blackjack/application/BlackjackRoomActor.java
@@ -1,16 +1,21 @@
 package org.Blackjack.application;
 
+import org.Blackjack.application.command.CreateRoom;
+import org.Blackjack.application.response.RoomSessionCreatedResponse;
 import org.Blackjack.domain.Dealer;
 import org.Blackjack.domain.Player;
 import org.Blackjack.domain.PlayerID;
 import org.Blackjack.infrastructure.AbstractActor;
 import org.Blackjack.infrastructure.Command;
 import org.Blackjack.infrastructure.Response;
+import org.Blackjack.infrastructure.SuccessResponse;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public final class BlackjackRoomActor extends AbstractActor {
+
+
     private final Dealer dealer;
     private final BlackjackTable table;
     private final Map<PlayerID, Player> players;
@@ -25,14 +30,14 @@ public final class BlackjackRoomActor extends AbstractActor {
     }
 
     @Override
-    public Response onReceive(Command message) {
-        return switch (message) {
-            case CreateSession createSession -> onCreateSessionCommand(createSession);
-            default -> throw new IllegalStateException("Unexpected value: " + message);
+    public Response onReceive(Command command) {
+        return switch (command.message()) {
+            case CreateRoom createRoom -> onCreateSessionCommand(createRoom);
+            default -> throw new IllegalStateException("Unexpected value: " + command.message());
         };
     }
 
-    private SessionCreated onCreateSessionCommand(CreateSession command) {
+    private Response onCreateSessionCommand(CreateRoom command) {
 
         if(gameRoomState != GameRoomState.ROOM_CREATED) {
             throw new IllegalStateException("Room state must be ROOM_CREATED");
@@ -43,8 +48,7 @@ public final class BlackjackRoomActor extends AbstractActor {
         players.putIfAbsent(command.player().id(), command.player());
 
         gameRoomState = GameRoomState.WAITING_PLAYERS;
-
-        return new SessionCreated(roomOwnerId, id() , gameRoomState);
+        return new SuccessResponse<>(new RoomSessionCreatedResponse(roomOwnerId, id() , gameRoomState));
     }
 
 }
