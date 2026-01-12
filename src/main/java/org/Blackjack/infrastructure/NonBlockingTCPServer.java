45f22364c386c40fb402f8953a9aa17ea6cad55f
Hasancan Sahan <yamahasirossi@gmail.com>
2026-01-12 16:01:37 +0300

    unnecessary code blocks deleted and refactored

--- src/main/java/org/Blackjack/infrastructure/NonBlockingTCPServer.java
+++ src/main/java/org/Blackjack/infrastructure/NonBlockingTCPServer.java
@@ -1,38 +1,25 @@
 package org.Blackjack.infrastructure;
 
-import org.Blackjack.application.BlackjackTableManagerActor;
-import org.Blackjack.application.PlayerManagerActor;
-import org.Blackjack.application.RoomId;
-import org.Blackjack.application.command.CreateRoom;
-import org.Blackjack.application.command.IsUserExist;
-import org.Blackjack.application.command.SavePlayer;
-import org.Blackjack.application.response.RoomCreatedAndRegistered;
-import org.Blackjack.domain.Player;
-import org.Blackjack.domain.PlayerID;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ConcurrentMap;
 
 public class NonBlockingTCPServer {
     private final int port;
     private Selector selector;
     private ServerSocketChannel serverChannel;
     private Set<SocketChannel> connections = ConcurrentHashMap.newKeySet();
-    private final ActorSystem system = new ActorSystem();
-    private final ActorRef managerRef = system.fork(new BlackjackTableManagerActor());
-    private final ActorRef playerManagerRef = system.fork(new PlayerManagerActor());
-    private final Queue<Runnable> pendingTasks = new ConcurrentLinkedQueue<>();
-    public NonBlockingTCPServer(int port) {
+    private SelectorExecutor selectorExecutor;
+    private Transport transport;
+    private ProtocolHandler protocolHandler;
+    public NonBlockingTCPServer(Transport transport, ProtocolHandler protocolHandler, int port) {
+        this.transport = transport;
+        this.protocolHandler = protocolHandler;
         this.port = port;
     }
 
@@ -43,7 +30,7 @@ public class NonBlockingTCPServer {
 
             while (true) {
                 selector.select();
-                runPendingTasks();
+                selectorExecutor.executeTasks();
                 Set<SelectionKey> selectedKeys = selector.selectedKeys();
                 Iterator<SelectionKey> iter = selectedKeys.iterator();
 
@@ -65,22 +52,10 @@ public class NonBlockingTCPServer {
             closeServer();
         }
     }
-    private void broadcast(byte[] data) {
-        for (SocketChannel client : connections) {
-            SelectionKey key = client.keyFor(selector);
-            if (key == null || !key.isValid()) continue;
 
-            ByteBuffer buffer = ByteBuffer.wrap(data);
-
-            key.attach(buffer);
-            key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
-        }
-
-        selector.wakeup();
-
-    }
     private void initServer() throws IOException {
         selector = Selector.open();
+        selectorExecutor = new SelectorExecutor(selector, new ConcurrentLinkedQueue<>());
         serverChannel = ServerSocketChannel.open();
         serverChannel.configureBlocking(false);
         serverChannel.bind(new InetSocketAddress(port));
@@ -93,145 +68,46 @@ public class NonBlockingTCPServer {
         connections.add(client);
         client.configureBlocking(false);
         var selectionKey = client.register(selector, SelectionKey.OP_READ);
-        selectionKey.attach(new ClientContext(client));
+        selectionKey.attach(new ClientContext(client, selectorExecutor, selectionKey));
         System.out.println("New client connected: " + client.getRemoteAddress());
     }
 
     private void handleRead(SelectionKey key) throws IOException {
-        SocketChannel client = (SocketChannel) key.channel();
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        ClientContext clientContext = (ClientContext) key.attachment();
 
-        int bytesRead = client.read(buffer);
+        TransportReadResponse result = transport.read(clientContext);
 
-        if (bytesRead == -1) {
-            pendingTasks.offer(() -> closeClient(key));
-            selector.wakeup();
-        } else if (bytesRead > 0) {
-            buffer.flip();
-            byte[] data = new byte[buffer.limit()];
-            buffer.get(data);
-            String message = new String(data).trim();
-            if(message.equals("EXIT")) {
-                pendingTasks.offer(() -> closeClient(key));
+        switch (result) {
+            case TransportReadResponse.Data(ByteBuffer data) -> {
+                protocolHandler.onData(clientContext, data);
+                key.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
                 selector.wakeup();
             }
-
-            else if (message.startsWith("REGISTER_PLAYER")) {
-                // <REGISTER_PLAYER:USERNAME>
-                playerManagerRef.send(new AsyncCommand<>(new SavePlayer(message.split(":")[1])))
-                        .whenComplete(((response, throwable) -> {
-                            pendingTasks.offer(() -> {
-                                ClientContext ctx = (ClientContext) key.attachment();
-
-                                if (throwable != null) {
-                                    ctx.writeQueue.add(ByteBuffer.wrap(("ERROR:"+throwable.getMessage()+"\n").getBytes()));
-                                }
-                                else if(response instanceof SuccessResponse<?> successResponse) {
-                                        Player player = (Player)successResponse.message();
-                                        String str = "USER_CREATED:" + " PLAYER_ID=" + player.id().id() + ":USERNAME=" +player.username()+"\n";
-                                        ctx.writeQueue.add(ByteBuffer.wrap(str.getBytes()));
-                                }
-                                else if(response instanceof ApplicationErrorResponse errorResponse) {
-                                    ctx.writeQueue.add(ByteBuffer.wrap(("ERROR_RESPONSE:"+errorResponse.message()+"\n").getBytes()));
-                                }
-
-                                key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
-
-                            });
-                            selector.wakeup();
-                        }));
+            case TransportReadResponse.NeedWrite _ -> {
+                key.interestOps(SelectionKey.OP_WRITE);
             }
-
-            else if(message.startsWith("CREATE_ROOM")){
-                // CREATE_ROOM:USERNAME
-                playerManagerRef.send(new AsyncCommand<>(new IsUserExist(message.split(":")[1])))
-                        .thenCompose(response -> {
-                            if (response instanceof SuccessResponse<?> success) {
-                                Player player = (Player) success.message();
-                                return managerRef.send(
-                                        new AsyncCommand<>(new CreateRoom(player))
-                                );
-                            }
-                            else  {
-                                if (response instanceof  ApplicationErrorResponse applicationErrorResponse) {
-                                    pendingTasks.offer(() -> {
-                                        ClientContext ctx = (ClientContext) key.attachment();
-                                        ctx.writeQueue.add(
-                                                ByteBuffer.wrap("USER NOT FOUND\n".getBytes())
-                                        );
-                                        key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
-                                    });
-                                    selector.wakeup();
-
-                                    return CompletableFuture.completedFuture(null);
-                                }
-                            }
-                            return null;
-                        }).thenAccept(response -> {
-                            if (response == null) {
-                                return;
-                            }
-
-                            pendingTasks.offer(() -> {
-                                ClientContext ctx = (ClientContext) key.attachment();
-
-                                if (response instanceof SuccessResponse<?> success) {
-                                    RoomCreatedAndRegistered payload =
-                                            (RoomCreatedAndRegistered) success.message();
-
-                                    String msg =
-                                            "ROOM CREATED : Room Id " + payload.uuid() +
-                                                    " : Player Id " + payload.playerID().id() + "\n";
-
-                                    ctx.writeQueue.add(ByteBuffer.wrap(msg.getBytes()));
-                                } else {
-                                    ctx.writeQueue.add(
-                                            ByteBuffer.wrap("ERROR\n".getBytes())
-                                    );
-                                }
-
-                                key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
-                            });
-
-                            selector.wakeup();
-                        });
-
+            case TransportReadResponse.Closed _ -> {
+                selectorExecutor.offerTask(() -> closeClient(key));
+                selector.wakeup();
             }
-
-            else {
-                ClientContext ctx = (ClientContext) key.attachment();
-                ctx.writeQueue.add(ByteBuffer.wrap("Unkonwn route message\n".getBytes()));
-                key.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
-            }
-            key.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
         }
+
     }
 
     private void handleWrite(SelectionKey key) throws IOException {
         ClientContext ctx = (ClientContext) key.attachment();
-        SocketChannel client = ctx.channel;
 
-        while (true) {
-            ByteBuffer buffer = ctx.writeQueue.peek();
+        TransportWriteResponse result = transport.write(ctx);
 
-            if (buffer == null) {
-                // yazacak hiçbir şey kalmadı
-                key.interestOps(key.interestOps() & ~SelectionKey.OP_WRITE);
-                return;
+        switch (result) {
+            case TransportWriteResponse.Partial _ ->
+                    key.interestOps(SelectionKey.OP_WRITE);
+            case TransportWriteResponse.Done _ ->
+                key.interestOps(SelectionKey.OP_READ);
+            case TransportWriteResponse.Closed _ -> {
+                selectorExecutor.offerTask(() -> closeClient(key));
+                selector.wakeup();
             }
-
-            int written = client.write(buffer);
-
-            if (written == -1) {
-                connections.remove(client);
-                client.close();
-                return;
-            }
-
-            if (buffer.hasRemaining()) {
-                return;
-            }
-            ctx.writeQueue.poll();
         }
 
     }
@@ -251,12 +127,4 @@ public class NonBlockingTCPServer {
             e.printStackTrace();
         }
     }
-    private void runPendingTasks() {
-        Runnable task;
-        while ((task = pendingTasks.poll()) != null) {
-            task.run();
-        }
-    }
-
-
 }
