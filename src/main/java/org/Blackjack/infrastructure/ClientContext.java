a55d4edc18ee1b4bb14ff3fff436b1b21a74d8e4
Hasancan Sahan <yamahasirossi@gmail.com>
2026-01-12 16:01:08 +0300

    new methods added

--- src/main/java/org/Blackjack/infrastructure/ClientContext.java
+++ src/main/java/org/Blackjack/infrastructure/ClientContext.java
@@ -1,16 +1,40 @@
 package org.Blackjack.infrastructure;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Queue;
 
 public class ClientContext {
-    final SocketChannel channel;
-    final Queue<ByteBuffer> writeQueue = new ArrayDeque<>();
+    public final SocketChannel channel;
+    public final ByteBuffer readBuffer  = ByteBuffer.allocate(4096);
+    public final Queue<ByteBuffer> writeQueue = new ArrayDeque<>();
+    private final SelectorExecutor selectorExecutor;
+    private final SelectionKey key;
 
-    ClientContext(SocketChannel channel) {
+    ClientContext(SocketChannel channel, SelectorExecutor selectorExecutor, SelectionKey key) {
         this.channel = channel;
+        this.selectorExecutor = selectorExecutor;
+        this.key = key;
     }
+
+    public void writeLater(ByteBuffer buffer) {
+        selectorExecutor.offerTask(() -> {
+            writeQueue.add(buffer);
+            key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
+        });
+    }
+
+    public void closeLater() {
+        selectorExecutor.offerTask(() -> {
+            try {
+                key.cancel();
+                channel.close();
+            } catch (IOException ignored) {}
+        });
+    }
+
 }
