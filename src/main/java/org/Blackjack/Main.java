eb7c2696428bca797262d61a8428c0cd20d12790
Hasancan Sahan <yamahasirossi@gmail.com>
2026-01-12 16:01:42 +0300

    Update Main.java

--- src/main/java/org/Blackjack/Main.java
+++ src/main/java/org/Blackjack/Main.java
@@ -1,10 +1,13 @@
 package org.Blackjack;
 
+
+import org.Blackjack.infrastructure.BlackjackProtocolHandler;
 import org.Blackjack.infrastructure.NonBlockingTCPServer;
+import org.Blackjack.infrastructure.PlainTextTransport;
 
 public class Main {
     public static void main(String[] args) {
-        var tcp = new NonBlockingTCPServer(9090);
+        var tcp = new NonBlockingTCPServer(new PlainTextTransport(), new BlackjackProtocolHandler(),9090);
         tcp.start();
     }
 }
