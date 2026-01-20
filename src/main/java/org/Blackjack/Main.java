8cc573bbdf6142d649153ec1dadf608aa5ba164d
Hasancan Sahan <yamahasirossi@gmail.com>
2026-01-20 14:18:22 +0300

    Update Main.java

--- src/main/java/org/Blackjack/Main.java
+++ src/main/java/org/Blackjack/Main.java
@@ -1,13 +1,19 @@
 package org.Blackjack;
 
 
-import org.Blackjack.infrastructure.BlackjackProtocolHandler;
-import org.Blackjack.infrastructure.NonBlockingTCPServer;
-import org.Blackjack.infrastructure.PlainTextTransport;
+import org.Blackjack.application.BlackjackTableManagerActor;
+import org.Blackjack.application.PlayerManagerActor;
+import org.Blackjack.infrastructure.*;
 
 public class Main {
     public static void main(String[] args) {
-        var tcp = new NonBlockingTCPServer(new PlainTextTransport(), new BlackjackProtocolHandler(),9090);
-        tcp.start();
+//        var tcp = new NonBlockingTCPServer(new PlainTextTransport(), new BlackjackProtocolHandler(),9090);
+//        tcp.start();
+        var actorSystem = new ActorSystem();
+        var blackjackTableManagerActor = actorSystem.fork(new BlackjackTableManagerActor());
+        var playerManagerActor = actorSystem.fork(new PlayerManagerActor());
+
+        var tcp = actorSystem.fork(new TCPEventLoopActor(new PlainTextTransport(), new BlackjackProtocolHandler(blackjackTableManagerActor, playerManagerActor)));
+        tcp.send(new AsyncCommand<>(new IOEvent.Start(9090))).join();
     }
 }
